43eee7f125581c979c5155ee3c08bcef16a35772
Mikkel Kroman <mk@uplink.io>
2016-04-23 21:47:41 +0200

    Initial commit

---
+++ src/lib.rs
@@ -0,0 +1,25 @@
+// Copyright 2016 Mikkel Kroman <mk@uplink.io>
+//
+// Licensed under the Apache License, Version 2.0 <LICENSE-APACHE or
+// http://www.apache.org/licenses/LICENSE-2.0> or the MIT license
+// <LICENSE-MIT or http://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+#![feature(custom_derive, plugin)]
+#![plugin(serde_macros)]
+extern crate url;
+extern crate hyper;
+#[macro_use] extern crate log;
+extern crate serde;
+extern crate serde_json;
+
+pub const API_HOST: &'static str = "api.soundcloud.com";
+
+pub mod client;
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+    }
+}
